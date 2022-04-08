0f30f26a0d64d79045b6f6f0ffb9004e5f1f16c4
Qiuhong Anna Wei <54920263+QiuhongAnnaWei@users.noreply.github.com>
2022-04-08 00:53:30 -0400

    cleaned code

--- 2dsimulator.py
+++ 2dsimulator.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import numpy as np
 import random
+import math
 
 WIDTH = 1000
 HEIGHT = 1000
@@ -80,14 +81,11 @@ def tablechair_symx_rot():
     return Scene
 
 
-
-import math
-def tablechair_symx_num():
+def gen_clean_tablechair(rot=False):
     """Arrangement of 1 table and 4 chairs that is symmetric across the x axis"""
     Scene = np.ones((HEIGHT, WIDTH, 3), np.uint8) * 255
     # Table
     table_w, table_h = random.randint(400, 800) , 140
-    print("table_w = ", table_w)
     table_tl= ((WIDTH-table_w)/2,(HEIGHT-table_h)/2)# 300, 430
     table_br = (table_tl[0]+table_w,table_tl[1]+table_h)
     table_tr = (table_br[0], table_tl[1])
@@ -95,29 +93,20 @@ def tablechair_symx_num():
     radius = random.randint(30,50)
     mindist_chairs = 20 # distance between the circumference
     maxnumpairs = math.floor((table_w + mindist_chairs)/(2*radius + mindist_chairs))
-    # print("maxnumpairs = ", maxnumpairs)
     # minnumpairs = math.ceil(table_w/(2*radius + mindist_chairs + radius*3)) # radius*3 is the max distance between chairs
-    minnumpairs = math.ceil(0.4*table_w/(radius*2))
-    # print("minnumpairs = ", minnumpairs)
+    minnumpairs = math.ceil(table_w/(radius*2) *0.4)
     numpairs = random.randint(minnumpairs,maxnumpairs) #1-3
-    # offsets_x = np.random.randint(0, 25, size=numpairs)
     offsets_x = np.random.random(size=numpairs+1)
     offsets_x_exp = [math.exp(e) for e in offsets_x]
     offsets_x = np.array([e/sum(offsets_x_exp) for e in offsets_x_exp])* (table_w - numpairs*2*radius - (numpairs-1)*mindist_chairs) # softmax
-    print(offsets_x)
     offsets_y = np.random.randint(-0.5*mindist_chairs, 0.5*mindist_chairs, size=numpairs)
-    print(offsets_y)
-    degree = 0 #np.random.randint(0, 180)
-    # print("# degree =", degree)
+    degree = np.random.randint(0, 180) if rot else 0
     pts = np.array([[rot_degree(degree, table_tl[0], table_tl[1]), rot_degree(degree, table_tr[0], table_tr[1]), \
-           rot_degree(degree, table_br[0], table_br[1]), rot_degree(degree, table_bl[0], table_bl[1])]])
+                     rot_degree(degree, table_br[0], table_br[1]), rot_degree(degree, table_bl[0], table_bl[1])]])
     cv2.fillPoly(Scene, pts, (0, 255, 0))
-    # pre_rot_xs = []
-    offsetted_circlecenter = table_tl[0]+radius
+    offsetted_circlecenter = table_tl[0]+radius+offsets_x[0]
     for j in range(numpairs): # number of pairs
-        if j == 0:
-            offsetted_circlecenter += offsets_x[j]
-        else:
+        if j > 0:
             offsetted_circlecenter += 2*radius + mindist_chairs + offsets_x[j]
         cv2.circle(Scene, tuple(rot_degree(degree, offsetted_circlecenter, table_tl[1]-radius-mindist_chairs-offsets_y[j])), 
             radius, (0, 0, 255), cv2.FILLED)
@@ -125,24 +114,17 @@ def tablechair_symx_num():
             radius, (0, 0, 255), cv2.FILLED)
     return Scene
 
-
-
-# 1. different table sizes 2. diff chair sizes 3. diff number of chairs 4. more random positioning
-
 def rot_degree(degree, x, y, origin=[WIDTH/2, HEIGHT/2]):
-    """"Rotate x and y by degree in counterclockwise direction with center of rotation being origin. Returns numpy array."""
+    """Rotate x and y by degree in counterclockwise direction with center of rotation being origin. Returns numpy array."""
     # print(x, y)
     theta = np.radians(degree)
     R = np.array(( (np.cos(theta), -np.sin(theta)), (np.sin(theta), np.cos(theta)) ))
     rot_xy = np.dot(R,np.array((x-origin[0],y-origin[1])))
-    # rot_xy = [int(rot_xy[0].item()+origin[0]), int(rot_xy[1].item()+origin[1])]
     rot_xy = [int(rot_xy[0].item()+origin[0]), int(rot_xy[1].item()+origin[1])]
-    # x = tuple([int(e.item()) for e in rot_xy])
-    # print(rot_xy)
     return rot_xy
 
-if __name__ == '__main__':
-    Scene = tablechair_symx_num()
+def gen_data_sample():
+    Scene = gen_clean_tablechair()
     cv2.namedWindow('Scene')
     while True:
         cv2.imshow('Scene', Scene)
@@ -151,3 +133,6 @@ if __name__ == '__main__':
             cv2.imwrite('scene.png', Scene)
         if Key == 27:
             break
+
+if __name__ == '__main__':
+    gen_data_sample()
