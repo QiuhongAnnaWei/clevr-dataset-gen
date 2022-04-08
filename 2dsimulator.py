7e543539e38b2c1f2eae1d66a372af69780aa9c2
Qiuhong Anna Wei <54920263+QiuhongAnnaWei@users.noreply.github.com>
2022-04-08 11:31:37 -0400

    finished 2dsimulator. can directly run

--- 2dsimulator.py
+++ 2dsimulator.py
@@ -5,6 +5,7 @@ import argparse
 import numpy as np
 import random
 import math
+import time
 
 WIDTH = 1000
 HEIGHT = 1000
@@ -122,27 +123,35 @@ def tablecenter_2_pt(center, table_w, table_h):
                           [center[0]-table_w/2, center[1]+table_h/2]], dtype=np.int32)
     return table_pts  
 
-def build_scene(centers, table_w, table_h, radius, degree):
-    """All given centers (np array) are not yet rotated."""
+def build_rot_scene(unrotcenters, table_w, table_h, radius, degree):
+    """Given unrotated centers, build rotated cv2 scene return rotated copy of the centers"""
     Scene = np.ones((HEIGHT, WIDTH, 3), np.uint8) * 255
-    table_pts = tablecenter_2_pt(centers[0, :], table_w, table_h)
+    table_pts = tablecenter_2_pt(unrotcenters[0, :], table_w, table_h)
     table_pts = np.array([rot_degree(degree, p[0], p[1]) for p in table_pts], dtype=np.int32)
     cv2.fillPoly(Scene, [table_pts], (0, 255, 0))
-    for c in centers[1:]:
-        cv2.circle(Scene, tuple(rot_degree(degree, c[0], c[1])), radius, (0, 0, 255), cv2.FILLED)
-    return Scene
+    rotcenters = np.zeros(unrotcenters.shape,dtype=np.int32)
+    rotcenters[0] = unrotcenters[0]
+    for i in range(1, unrotcenters.shape[0]):
+        rotcenters[i] = rot_degree(degree, unrotcenters[i, 0], unrotcenters[i, 1])
+        cv2.circle(Scene, tuple(rotcenters[i]), radius, (0, 0, 255), cv2.FILLED)
+    if degree==0:
+        degposes=np.zeros((rotcenters.shape[0], 1)) -1
+        degposes[0, 0] = 0
+    else:
+        degposes=np.zeros((rotcenters.shape[0], 1))+degree
+    rotcenters_degrees = np.concatenate((rotcenters, degposes), axis=1)
+    return Scene, rotcenters_degrees
 
-def gen_tablechair(rot=False, to_perturb=True):
+def gen_tablechair(numpairs=None, radius=None, rot=False, to_perturb=True):
     """Arrangement of 1 table and 4 chairs that is symmetric across the x axis"""
-    table_w, table_h = random.randint(400, 800), 140
+    radius = radius if radius else random.randint(30,50) 
+    table_w = random.randint(400, numpairs*radius*2/0.4) if numpairs else random.randint(400, 800)
+    table_h = 140
     table_center = [[int(WIDTH/2), int(HEIGHT/2)]] # 1x2 (center of rotation)
-
-    radius = random.randint(30,50)
     mindist_chairs = 20 # distance between the circumference
     maxnumpairs = math.floor((table_w + mindist_chairs)/(2*radius + mindist_chairs))
-    # minnumpairs = math.ceil(table_w/(2*radius + mindist_chairs + radius*3)) # radius*3 is the max distance between chairs
-    minnumpairs = math.ceil(table_w/(radius*2) *0.4)
-    numpairs = random.randint(minnumpairs,maxnumpairs) #1-3
+    minnumpairs = math.ceil(table_w/(radius*2) *0.4) # same 0.4 as table_w above
+    numpairs = numpairs if numpairs else random.randint(minnumpairs,maxnumpairs) 
     degree = np.random.randint(0, 180) if rot else 0
 
     offsets_x = np.random.random(size=numpairs+1)
@@ -150,28 +159,30 @@ def gen_tablechair(rot=False, to_perturb=True):
     offsets_x = np.array([e/sum(offsets_x_exp) for e in offsets_x_exp])* (table_w - numpairs*2*radius - (numpairs-1)*mindist_chairs) # softmax
     offsets_y = np.random.randint(-0.5*mindist_chairs, 0.5*mindist_chairs, size=numpairs)
     chair_centers = chairoffset_2_center(table_center[0], table_w, table_h, offsets_x, offsets_y, radius, mindist_chairs) # list
-    centers = np.concatenate((np.array(table_center), np.array(chair_centers)), axis=0) # nx2
-    Scene = build_scene(centers, table_w, table_h, radius, degree)
+    unrotcenters = np.concatenate((np.array(table_center), np.array(chair_centers)), axis=0) # nx2
     if to_perturb:
-        per_Scene, per_centers = perturb(centers, table_w, table_h, radius, degree)
-    return Scene, centers, per_Scene, per_centers
+        per_unrotcenters = perturb(np.copy(unrotcenters), table_w, table_h, radius, degree)
+    Scene, rotcenters_degrees = build_rot_scene(unrotcenters, table_w, table_h, radius, degree)
+    per_Scene, per_rotcenters_degrees = build_rot_scene(per_unrotcenters, table_w, table_h, radius, degree)
 
+    shapes = np.repeat([[2,radius,radius]], (numpairs*2+1), axis=0)
+    shapes[0] = [1, table_w, table_h] 
+    return Scene, rotcenters_degrees, per_Scene, per_rotcenters_degrees, shapes
 
-def perturb(centers, table_w, table_h, radius, degree=0):
-    """All given centers (np array) are not yet rotated."""
+    
+
+def perturb(unrotcenters, table_w, table_h, radius, degree=0):
+    """Given unortated center (np array), return unrotated perturbed centers"""
     # translation perturbation (applied to center of table and whole scene)
-    centers += np.random.normal(0,30,2)
-    random_indices = list(range(1, centers.shape[0]-1))
+    unrotcenters += np.random.normal(0,30,2)
+    random_indices = list(range(1, unrotcenters.shape[0]-1))
     random.shuffle(random_indices) # in-place
     for chair_i in random_indices:
-        new_chair_cen = centers[chair_i] + np.random.normal(0,30,2)
-        while (not is_valid(centers, chair_i, new_chair_cen, radius, table_w, table_h)):
-            new_chair_cen = centers[chair_i] + np.random.normal(0,30,2)
-        centers[chair_i] = new_chair_cen
-    # np.random.normal(0,1,(3,2))
-    centers = np.array([rot_degree(degree, c[0], c[1]) for c in centers], dtype=np.int32)
-    Scene = build_scene(centers, table_w, table_h, radius, degree)
-    return Scene, centers
+        new_chair_cen = unrotcenters[chair_i] + np.random.normal(0,30,2)
+        while (not is_valid(unrotcenters, chair_i, new_chair_cen, radius, table_w, table_h)):
+            new_chair_cen = unrotcenters[chair_i] + np.random.normal(0,30,2)
+        unrotcenters[chair_i] = new_chair_cen
+    return unrotcenters
 
 def is_valid(centers, chair_i, new_chair_cen, radius, table_w, table_h):
     """checks if new_chair_cen for the ith chair is valid: no overlap with any other object"""
@@ -186,38 +197,43 @@ def is_valid(centers, chair_i, new_chair_cen, radius, table_w, table_h):
             return False
     return True
 
-def gen_data(n, rot=False):
+def gen_data(numinstances=10, numpairs=3, radius=50, rot=False, show=False, dir="./2dsimulator_data", save=True, start_idx=0):
     """Generate n data samples:
     1. per_poses, per_shape: n x dim numpy array, where ith row describes pose/shape of the ith perturbed scene. (1st row=table)
     2. clean_poses, clean_shape: n x dim numpy array, where ith row describes pose/shape of the ith clean scene (corresponds to ith perturbed scene)
     3. Images of clean and pertubed scene for reference
     """
     pose_d = 3 # center, rotation
-    shape_d = 2
-    # Chair: center, -1 (rotation) | type, radius (fixed), radius (fixed)
-    # Table: center, rotation,     | type, width (fixed),  height (fixed)
-    clean_poses, per_poses = np.zeros((n,pose_d)), np.zeros((n,pose_d))
-    clean_shape, per_shape = np.zeros((n,shape_d)),  np.zeros((n,shape_d))
+    shape_d = 3
+    # Table: center, rotation,     | type=1, width (fixed),  height (fixed)
+    # Chair: center, -1 (rotation) | type=2, radius (fixed), radius (fixed)
+    clean_poses, per_poses = np.zeros((numinstances,pose_d*(numpairs*2+1))), np.zeros((numinstances,pose_d*(numpairs*2+1)))
+    clean_shapes, per_shapes = np.zeros((numinstances,shape_d*(numpairs*2+1))), np.zeros((numinstances,shape_d*(numpairs*2+1)))
 
-    Scene, centers, per_Scene, per_centers = gen_tablechair(rot=rot, to_perturb=True)
-    # clean_poses[:,0:2] = centers # flatten
-    # per_poses[:,0:2] = per_centers
-    show_image(Scene)
-    show_image(per_Scene)
-    # TODO: file save and image write (cv2.imwrite('scene.png', Scene))
-    # TODO: poses: flatten + rotation
-    # if rot:
-    #     pass 
-    # else:
-    #     clean_poses[0,2] = 0 # table's rotation is 0
-    #     clean_poses[:,2] = -1 # chairs' rotation is -1
-    #     per_poses[0,2] = 0 # table's rotation is 0
-    #     per_poses[:,2] = -1 # chairs' rotation is -1
-    print("clean_poses:\n", clean_poses)
-    print("per_poses:\n", per_poses)
+    timestamp = str(int(time.time()))
+    print("timestamp =", timestamp)
+    savedir = os.path.join(dir, timestamp)
+    if not os.path.exists(savedir): os.makedirs(savedir)
+    for ins_i in range(numinstances):
+        Scene, rotcenters_degrees, per_Scene, per_rotcenters_degrees, shapes = gen_tablechair(numpairs, radius, rot=rot, to_perturb=True)
+        if show: show_image(Scene)
+        if show: show_image(per_Scene)
+        if save: cv2.imwrite(os.path.join(savedir, f"image{ins_i+start_idx:05d}_clean.png"), Scene)
+        if save: cv2.imwrite(os.path.join(savedir, f"image{ins_i+start_idx:05d}_pert.png"), per_Scene)
+        clean_poses[ins_i,:] = rotcenters_degrees.flatten()
+        per_poses[ins_i,:] = per_rotcenters_degrees.flatten()
+        clean_shapes[ins_i,:] = shapes.flatten()
+        per_shapes[ins_i,:] = shapes.flatten()
 
-    # TODO: clean_shape, per_shape
+    # print("clean_poses:\n", clean_poses)
+    # print("per_poses:\n", per_poses)
+    # print("clean_shapes:\n", clean_shapes)
+    # print("per_shapes:\n", per_shapes)
+    if save: np.savetxt(os.path.join(savedir, f'poses_clean{start_idx:05d}.txt'), clean_poses, fmt='%d')
+    if save: np.savetxt(os.path.join(savedir, f'poses_pert{start_idx:05d}.txt'), per_poses, fmt='%d')
+    if save: np.savetxt(os.path.join(savedir, f'shapes_clean{start_idx:05d}.txt'), clean_shapes, fmt='%d')
+    if save: np.savetxt(os.path.join(savedir, f'shapes_pert{start_idx:05d}.txt'), per_shapes, fmt='%d')
 
 
 if __name__ == '__main__':
-    gen_data(1)
+    gen_data(numinstances=10, numpairs=3, radius=50, show=False, save=True, start_idx=0) # numinstances, numpairs, radius
